a1cda041e41cfcba0412459e197452eccfc63fdf
Josh Yered <charlesyered4@gmail.com>
2026-01-11 07:49:54 +0100

    views

--- marchB/listings/views.py
+++ marchB/listings/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1 style="color:green">Apropos</h1><br /> <p>Nous adoros cette app</p>')
+    

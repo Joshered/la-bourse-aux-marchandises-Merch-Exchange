8bb619993c5d145448d7030badd0a47f8635a424
Josh Yered <charlesyered4@gmail.com>
2026-06-04 10:40:20 +0200

    affichage de la bdd dans une view

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Band
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+                        <h1>Bonjour Django</h1>
+                        <p>Mes goupes preferer sont :</p>
+                        <ul>
+                        
+                            <li>{bands[0].name}</li>
+                            <li>{bands[1].name}</li> 
+                            <li>{bands[2].name}</li>   
+                        </ul>
+                        
+                        """)
 
 def about(request):
     return HttpResponse('<h1>Apropos</h2> <p>Nous adorons merch !</p>')

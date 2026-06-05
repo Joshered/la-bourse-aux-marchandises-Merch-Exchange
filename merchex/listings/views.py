e9f38560eed5a258ad4f9de9ab14fdfc8554ec19
Josh Yered <charlesyered4@gmail.com>
2026-06-05 10:00:36 +0200

    feat: des templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -6,15 +6,7 @@ from .models import Listings
 # POur les groupe.
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-                        <h1>Bonjour Django</h1>
-                        <p>Mes goupes preferer sont :</p>
-                        <ul>
-                            <li>{bands[0].name}</li>
-                            <li>{bands[1].name}</li> 
-                            <li>{bands[2].name}</li>   
-                        </ul>
-                        """)
+    return render(request,'listings/hello.html')
 
 def about(request):
     return HttpResponse('<h1>Apropos</h2> <p>Nous adorons merch !</p>')

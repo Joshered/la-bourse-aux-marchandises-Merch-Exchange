4cab1455ac56ffcdda430d504664ab9b5f665479
Josh Yered <charlesyered4@gmail.com>
2026-06-05 13:15:45 +0200

    docs(views): commentaire

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,18 +3,20 @@ from django.shortcuts import render
 from .models import Band
 from .models import Listings
 
-# POur les groupe.
+# POUR LA PAGE D'ACCUEIL
 def hello(request):
     bands = Band.objects.all()
     return render(request,'listings/hello.html',{"bands":bands})
 
+# POUR LES PAGES ABOUT
 def about(request):
     return render(request,'listings/about.html')
 
+# POUR LES PAGES CONTACT
 def contact(request):
     return render(request,'listings/contact.html')
 
-# POUR LES LISTings
+# POUR LES LISTINGS
 def listings(request):
     listing = Listings.objects.all()
     return render(request,'listings/listings.html',{"listings":listing})
